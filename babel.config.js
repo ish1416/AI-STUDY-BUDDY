fef8331c0221d50347b986edac711f26b9952b69
ish1416 <ish1442006@gmail.com>
2025-11-21 17:03:33 +0530

    fix: install babel-preset-expo and update babel config for dotenv

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,13 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       ['module:react-native-dotenv', {
+        envName: 'APP_ENV',
         moduleName: '@env',
         path: '.env',
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: true,
       }]
     ]
   };
